1bc988732008bf62d1d4f15cfcfde16c2ddb45fd
V.G. Bulavintsev <golem.md@gmail.com>
2019-08-19 18:06:54 +0200

    Attempt4

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 setup(
     name='pre_commit_dummy_package',
     version='0.0.0',
-    install_requires=['git+https://github.com/Tribler/isort@isort_tribler'],
+    install_requires=['git+https://github.com/Tribler/isort@4.3.21-2-tribler#isort'],
 )
